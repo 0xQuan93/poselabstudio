cc2ba71055fbe7f5e0580b683b9baba8ec891c9a
0xQuan <qxgdcl@gmail.com>
2026-02-26 11:59:56 -0600

    feat: add loading state to chat messages in StudioChatPanel

--- src/components/studio/StudioChatPanel.tsx
+++ src/components/studio/StudioChatPanel.tsx
@@ -166,24 +166,28 @@ export const StudioChatPanel = () => {
       </div>
       
       <div className="chat-messages">
-        {messages.map((msg) => (
-          <div key={msg.id} className={`message ${msg.author.username === user?.username ? 'own' : ''}`}>
-            <div className="message-avatar">
-              {msg.author.avatarUrl ? (
-                <img src={msg.author.avatarUrl} alt={msg.author.username} />
-              ) : (
-                <div className="avatar-placeholder">{msg.author.username[0]}</div>
-              )}
-            </div>
-            <div className="message-content">
-              <div className="message-header">
-                <span className="username">{msg.author.username}</span>
-                <span className="timestamp">{msg.timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+        {isLoading && messages.length === 0 ? (
+          <div className="chat-loading">Loading messages...</div>
+        ) : (
+          messages.map((msg) => (
+            <div key={msg.id} className={`message ${msg.author.username === user?.username ? 'own' : ''}`}>
+              <div className="message-avatar">
+                {msg.author.avatarUrl ? (
+                  <img src={msg.author.avatarUrl} alt={msg.author.username} />
+                ) : (
+                  <div className="avatar-placeholder">{msg.author.username[0]}</div>
+                )}
               </div>
-              <p>{msg.content}</p>
+              <div className="message-content">
+                <div className="message-header">
+                  <span className="username">{msg.author.username}</span>
+                  <span className="timestamp">{msg.timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+                </div>
+                <p>{msg.content}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
         <div ref={messagesEndRef} />
       </div>
 
